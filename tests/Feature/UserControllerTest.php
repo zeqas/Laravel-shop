3bb01ca0fbf697500dd4964ef5f0d28ae26258eb
zeqas <casperi52400@gmail.com>
2024-04-15 17:27:09 +0800

    test: 新增使用者測試

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @covers App\Http\Controllers\UserController::register
+     * 註冊 成功
+     */
+    public function test_register_success()
+    {
+        // 發送請求
+        $response = $this->postJson("api/register", [
+            'name' => 'tester',
+            'email' => 'tester@mail.com',
+            'password' => 'hashedPassword',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'id',
+        ]);
+
+        // 檢查資料庫
+        $this->assertDatabaseHas('users', [
+            'name' => 'tester',
+            'email' => 'tester@mail.com',
+        ]);
+    }
+
+    /**
+     * @covers App\Http\Controllers\UserController::login
+     * 登入 成功
+     */
+    public function test_login_success()
+    {
+        $userRegisterData = [
+            'name' => 'tester',
+            'email' => 'tester@mail.com',
+            'password' => 'hashedPassword'
+        ];
+
+        // 建立用戶
+        $this->postJson("api/register", $userRegisterData);
+
+        // 發送請求
+        $response = $this->postJson("api/login", [
+            'email' => 'tester@mail.com',
+            'password' => 'hashedPassword'
+        ]);
+
+        // 檢查狀態碼和回傳內容是否有 id 和 token
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'id',
+            'token',
+        ]);
+    }
+
+    /**
+     * @covers App\Http\Controllers\UserController::login
+     * 登入 失敗
+     */
+    public function test_login_fail()
+    {
+        // 發送請求
+        $response = $this->postJson("api/login", [
+            'email' => 'test@mail.wrong.com',
+            'password' => '123',
+        ]);
+
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message' => 'Invalid credentials',
+        ]);
+    }
+
+    /**
+     * @covers App\Http\Controllers\UserController::me
+     * 取得使用者資訊 成功
+     */
+    public function test_me_success()
+    {
+        // 建立用戶
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->getJson("api/me")
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => $user->id,
+                'email' => $user->email,
+            ]);
+    }
+
+    /**
+     * @covers App\Http\Controllers\UserController::me
+     * 取得使用者資訊 無效 token 失敗
+     */
+    public function test_me_fail()
+    {
+        $this->getJson("api/me")
+            ->assertStatus(401)
+            ->assertJson([
+                "success" => false,
+                "msg" => "無效Token",
+            ]);
+    }
+}
